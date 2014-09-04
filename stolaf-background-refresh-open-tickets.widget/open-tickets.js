03a4fef4f364aa5872eee1e6c1b21b3c9a29f4cd
Hawken Rives <hawkrives@gmail.com>
2014-09-04 07:17:36 -0500

    Store the data in localStorage, instead of fetching it for every widget.

---
+++ stolaf-background-refresh-open-tickets.widget/open-tickets.js
@@ -0,0 +1,19 @@
+command: [
+	'curl --silent ',
+	'"https://help.stolaf.edu/helpdesk',
+	'/WebObjects/Helpdesk.woa/ra/Tickets?',
+	'style=details&',
+	'qualifier=(statustype.statusTypeName%3D%27Open%27)&',
+	'limit=50&',
+	'apiKey=dYqqjOjwkOd2k411ytv8G6rADIgpCbWZ59WouoEc"',
+].join(''),
+
+refreshFrequency: 60000,
+
+render: function(argument) {
+	return "";
+},
+
+update: function(output, domEl) {
+	localStorage.setItem('stolaf-closed-tickets', output);
+}
