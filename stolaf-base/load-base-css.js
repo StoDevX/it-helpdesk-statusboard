fff4694e74c638188bc0bdcfdcfcdc4652bb6370
Hawken Rives <hawkrives@gmail.com>
2014-09-06 15:54:39 -0500

    Load css files from a list

--- stolaf-base/load-base-css.js
+++ stolaf-base/load-base-css.js
@@ -2,7 +2,9 @@ command: 'echo "nothing"',
 
 updateFrequency: 600000,
 
-style: '@import "/stolaf-base/base.css"',
+style: [
+	'@import "/stolaf-base/base.css"',
+].join('\n'),
 
 render: function(output) {
 	return ''
