ba89b15fdbff93798d155638744b779c52873c6f
Hawken Rives <hawkrives@gmail.com>
2014-10-14 16:29:42 -0500

    Revert "First attempt at pulling the API key out of the source"
    This reverts commit 149476302438cf48940ab339bd0743b438aafd09.

--- stolaf-background.widget/closed-tickets.js
+++ stolaf-background.widget/closed-tickets.js
@@ -1,11 +1,11 @@
 command: [
-	'echo ./whd-api-key.txt | xargs curl --silent ',
+	'curl --silent ',
 		'"https://help.stolaf.edu/helpdesk',
 		'/WebObjects/Helpdesk.woa/ra/Tickets?',
 		'style=details&',
 		'qualifier=(statustype.statusTypeName%3D%27Closed%27)&',
 		'limit=50&',
-		'apiKey="',
+		'apiKey=***REMOVED***"',
 ].join(''),
 
 refreshFrequency: 60000,
@@ -15,33 +15,16 @@ style: [
 	"bottom: 0",
 	"left: 25%",
 	"width: 25%",
-	"height: 3%",
-
+	"text-align: center",
 	"border: 0",
+	"height: 3%",
+	"vertical-align: middle",
 	"color: rgba(255, 255, 255, 0.5)",
 	"font-weight: 300",
-
-	"display: flex",
-	"flex-flow: row nowrap",
-	"justify-content: center",
-	"align-items: center",
-
-	"main",
-	"	display: inline-block",
-
-	"button",
-	"	color: inherit",
-	"	background: hsla(0, 0%, 90%, 0.25)",
-	"	border: 0",
-	"	border-radius: 50px",
-	"	margin-left: 1em"
 ].join('\n'),
 
 render: function(argument) {
-	return [
-		'<main class="center">Closed Tickets: <span class="last-updated"></span></main>',
-		//'<button>↻</button>',
-	].join('');
+	return 'Closed Tickets: <span class="last-updated"></span>';
 },
 
 update: function(output, domEl) {
