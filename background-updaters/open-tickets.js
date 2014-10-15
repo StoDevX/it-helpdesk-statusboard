35162a4346acf4d6fd417d4be3fac3d1cd2a99e6
Hawken Rives <hawkrives@gmail.com>
2014-10-15 11:47:37 -0500

    Update closedTickets and openTickets to use the new output of the scripts

--- background-updaters/open-tickets.js
+++ background-updaters/open-tickets.js
@@ -25,8 +25,9 @@ update: function(output, domEl) {
 	var moment = window.sto.libs.moment;
 	window.sto.data = window.sto.data || {};
 
-	window.sto.data.openTickets = JSON.parse(output);
+	var ticketData = JSON.parse(output);
+	window.sto.data.openTickets = ticketData.data;
 
-	this.lastUpdateTime = new Date();
+	this.lastUpdateTime = moment(ticketData.lastUpdated);
 	domEl.querySelector('.last-updated').textContent = moment(this.lastUpdateTime).calendar();
 },
