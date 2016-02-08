1d3e8572a1c5e244b6eacbdb32a4f2272bb2b8ae
Hawken Rives <hawkrives@gmail.com>
2016-02-08 13:16:34 -0600

    fix indentation

--- scripts/getTickets.py
+++ scripts/getTickets.py
@@ -6,30 +6,30 @@ import data_helpers
 
 
 def get_credentials():
-	# returns 'api-key'
-	with open('credentials/webhelpdesk.credential', 'r') as credentials:
-		return credentials.read()
+    # returns 'api-key'
+    with open('credentials/webhelpdesk.credential', 'r') as credentials:
+        return credentials.read()
 
 
 def main():
-	if len(sys.argv) >= 2:
-		statustype = sys.argv[1]
-		filename = '-'.join(statustype.lower().split()) + '-tickets.json'
-	else:
-		exit(1)
+    if len(sys.argv) >= 2:
+        statustype = sys.argv[1]
+        filename = '-'.join(statustype.lower().split()) + '-tickets.json'
+    else:
+        exit(1)
 
-	ticket_count = 300
+    ticket_count = 300
 
-	whd_tickets = 'https://help.stolaf.edu/helpdesk/WebObjects/Helpdesk.woa/ra/Tickets'
-	apiKey = get_credentials()
-	params = '?style=details&limit='+str(ticket_count)+'&qualifier=(statustype.statusTypeName%3D%27'+urllib.quote(statustype)+'%27)&apiKey='+apiKey
+    apiKey = get_credentials()
+    whd_tickets = 'https://help.stolaf.edu/helpdesk/WebObjects/Helpdesk.woa/ra/Tickets'
+    params = '?style=details&limit='+str(ticket_count)+'&qualifier=(statustype.statusTypeName%3D%27'+urllib.quote(statustype)+'%27)&apiKey='+apiKey
 
-	if not data_helpers.needs_reload(filename, minutes=1):
-		return ""
+    if not data_helpers.needs_reload(filename, minutes=1):
+        return ""
 
-	tickets = check_output('curl --silent "%s"' % (whd_tickets+params), shell=True)
+    tickets = check_output('curl --silent "%s"' % (whd_tickets+params), shell=True)
 
-	data_helpers.save_data(filename, json.loads(tickets))
+    data_helpers.save_data(filename, json.loads(tickets))
 
 if __name__ == '__main__':
-	main()
+    main()
