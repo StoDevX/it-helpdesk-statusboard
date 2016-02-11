2957e56efb0c097928a12c0fed9be211bda799a7
Hawken Rives <hawkrives@gmail.com>
2016-02-10 20:19:27 -0600

    turn off awaiting_client counts

--- lib/count_open_tickets.py
+++ lib/count_open_tickets.py
@@ -3,8 +3,8 @@ from .get_tickets import get_tickets
 
 def count_open_tickets():
     open_tickets = get_tickets('open')
-    awaiting_client_tickets = get_tickets('awaiting client')
-    return len(open_tickets) + len(awaiting_client_tickets)
+    # awaiting_client_tickets = get_tickets('awaiting client')
+    return len(open_tickets)  # + len(awaiting_client_tickets)
 
 
 if __name__ == '__main__':
