b8854056a7b003e2b0e4cff2538629321fd42395
Hawken Rives <hawkrives@gmail.com>
2016-03-28 18:12:24 -0500

    add flatten function

--- lib/functions.py
+++ lib/functions.py
@@ -19,3 +19,13 @@ def group_by(predicate, iterable):
     sorted_iterable = sorted(iterable, key=predicate)
     grouped = groupby(sorted_iterable, key=predicate)
     return {k: list(v) for k, v in grouped}
+
+
+def flatten(*args):
+    # from http://stackoverflow.com/a/2158532/2347774
+    for l in args:
+        for el in l:
+            if isinstance(el, list) and not isinstance(el, str):
+                yield from flatten(el)
+            else:
+                yield el
