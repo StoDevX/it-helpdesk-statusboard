89f1a9e3ab3e8a8a9cdbc7886af10d310960b551
Hawken Rives <hawkrives@gmail.com>
2016-05-04 10:57:36 -0500

    add main() functions to two widgets

--- fresh-tickets.py
+++ fresh-tickets.py
@@ -2,15 +2,23 @@ from lib.list_tickets import list_tickets
 from argparse import ArgumentParser
 import json
 
-if __name__ == '__main__':
+def main():
     parser = ArgumentParser(description='Find the top helpdesk responders')
     parser.add_argument('--json', action='store_true', default=False)
     parser.add_argument('--count', action='store', default=7, type=int)
     args = parser.parse_args()
 
-    tickets = list_tickets(args.count)
+    try:
+        tickets = list_tickets(args.count)
+    except Exception as err:
+        print(err)
+        return
+
     if args.json:
         print(json.dumps(tickets))
     else:
         for ticket in tickets:
             print(ticket)
+
+if __name__ == '__main__':
+    main()
--- top-responders.py
+++ top-responders.py
@@ -2,14 +2,22 @@ from lib.count_responders import count_responders
 from argparse import ArgumentParser
 import json
 
-if __name__ == '__main__':
+def main():
     parser = ArgumentParser(description='Find the top helpdesk responders')
     parser.add_argument('--json', action='store_true', default=False)
     args = parser.parse_args()
 
-    responders = count_responders()
+    try:
+        responders = count_responders()
+    except Exception as e:
+        print(e)
+        return
+
     if args.json:
         print(json.dumps(responders))
     else:
         for responder in responders:
             print("{1} {0}".format(*responder))
+
+if __name__ == '__main__':
+    main()
