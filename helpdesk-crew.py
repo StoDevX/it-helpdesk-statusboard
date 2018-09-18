c6eec9faa3340719eed9d9f2361418bc358f8aae
Hawken Rives <rives@stolaf.edu>
2018-09-18 09:24:36 -0500

    extract helpdesk-crew into function

--- helpdesk-crew.py
+++ helpdesk-crew.py
@@ -2,16 +2,22 @@ from lib.get_shifts import get_shifts
 from argparse import ArgumentParser
 import json
 
-if __name__ == '__main__':
+def main():
     parser = ArgumentParser(description='Show the helpdesk crew')
     parser.add_argument('--json', action='store_true', default=False)
     args = parser.parse_args()
 
+    shifts = get_shifts()
+
     if args.json:
-        print(json.dumps(get_shifts()))
+        print(json.dumps(shifts))
     else:
         print('Now @ Helpdesk:')
         print('Now @ TCAR:')
         print('Next @ Helpdesk:')
         print('Next @ TCAR:')
-        print(json.dumps(get_shifts()))
+        print(json.dumps(shifts))
+
+
+if __name__ == '__main__':
+    main()
